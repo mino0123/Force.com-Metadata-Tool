7cef7c46b3aa4fd822999a2825c4d3acf693e7a8
mino0123 <minoaw@gmail.com>
2013-01-17 18:39:03 +0900

    ファイル名変更

--- forcedotcommetadatatool.user.js
+++ forcedotcommetadatatool.user.js
@@ -173,6 +173,45 @@ GM_addStyle(
     '}'
 );
 var Template = {};
+Template.DeployRetrieve =
+    '<div class="mt-deploy-panel">' +
+    '    <table>' +
+    '        <tr>' +
+    '            <td><label for="mt-deploy-name">name:</label></td>' +
+    '            <td><input type="text" id="mt-deploy-name"></td>' +
+    '        </tr>' +
+    '        <tr>' +
+    '            <td><label for="mt-deploy-member">member:</label></td>' +
+    '            <td><input type="text" id="mt-deploy-member"></td>' +
+    '        </tr>' +
+    '        <tr>' +
+    '            <td><label for="mt-retrieve-file">file:</label></td>' +
+    '            <td><input type="text" id="mt-retrieve-file"></td>' +
+    '        </tr>' +
+    '        <tr>' +
+    '            <td><label for="mt-deploy-dir">directory:</label></td>' +
+    '            <td><input type="text" id="mt-deploy-dir"></td>' +
+    '        </tr>' +
+    '        <tr>' +
+    '            <td><label for="mt-deploy-basename">basename:</label></td>' +
+    '            <td><input type="text" id="mt-deploy-basename"></td>' +
+    '        </tr>' +
+    '        <tr>' +
+    '            <td><label for="mt-deploy-ext">extension:</label></td>' +
+    '            <td><input type="text" id="mt-deploy-ext"></td>' +
+    '        </tr>' +
+    '        <tr>' +
+    '            <td><label for="mt-retrieve-has-meta">meta file:</label></td>' +
+    '            <td><input type="checkbox" id="mt-retrieve-has-meta"></td>' +
+    '        </tr>' +
+    '    </table>' +
+    '    <button id="mt-retrieve">retrieve</button>' +
+    '    <button id="mt-deploy">deploy</button>' +
+    '    <br />' +
+    '    <textarea id="mt-deploy-content"></textarea>' +
+    '    <br />' +
+    '    <textarea id="mt-deploy-meta"></textarea>' +
+    '</div>';
 Template.GlobalDescribe =
     '<table class="mt-gd-table">' +
     '    {{#groups}}' +
@@ -333,45 +372,6 @@ Template.GeneralTableView =
     '    {{/rows}}' +
     '</table>';
 
-Template.DeployRetrieve =
-    '<div class="mt-deploy-panel">' +
-    '    <table>' +
-    '        <tr>' +
-    '            <td><label for="mt-deploy-name">name:</label></td>' +
-    '            <td><input type="text" id="mt-deploy-name"></td>' +
-    '        </tr>' +
-    '        <tr>' +
-    '            <td><label for="mt-deploy-member">member:</label></td>' +
-    '            <td><input type="text" id="mt-deploy-member"></td>' +
-    '        </tr>' +
-    '        <tr>' +
-    '            <td><label for="mt-retrieve-file">file:</label></td>' +
-    '            <td><input type="text" id="mt-retrieve-file"></td>' +
-    '        </tr>' +
-    '        <tr>' +
-    '            <td><label for="mt-deploy-dir">directory:</label></td>' +
-    '            <td><input type="text" id="mt-deploy-dir"></td>' +
-    '        </tr>' +
-    '        <tr>' +
-    '            <td><label for="mt-deploy-basename">basename:</label></td>' +
-    '            <td><input type="text" id="mt-deploy-basename"></td>' +
-    '        </tr>' +
-    '        <tr>' +
-    '            <td><label for="mt-deploy-ext">extension:</label></td>' +
-    '            <td><input type="text" id="mt-deploy-ext"></td>' +
-    '        </tr>' +
-    '        <tr>' +
-    '            <td><label for="mt-retrieve-has-meta">meta file:</label></td>' +
-    '            <td><input type="checkbox" id="mt-retrieve-has-meta"></td>' +
-    '        </tr>' +
-    '    </table>' +
-    '    <button id="mt-retrieve">retrieve</button>' +
-    '    <button id="mt-deploy">deploy</button>' +
-    '    <br />' +
-    '    <textarea id="mt-deploy-content"></textarea>' +
-    '    <br />' +
-    '    <textarea id="mt-deploy-meta"></textarea>' +
-    '</div>';
 Template.TabList =
     '<ul id="{{id}}" class="mt-tab-list">' +
     '    {{#tabs}}' +
